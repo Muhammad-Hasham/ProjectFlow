af5056ae3a61be158ddc801a10c46e6b879b132c
Khadija <i200760@nu.edu.pk>
2024-02-29 18:55:01 +0500

    alignments fixed

--- src/pages/ProjectVisualization/details.jsx
+++ src/pages/ProjectVisualization/details.jsx
@@ -7,9 +7,9 @@ import Navigation from 'pages/Sidebar';
 
 const ProjectProgress = ({ progress, statisticsData, tasks }) => {
 
-  const[projectname,setProjectname]=useState("");
+  const [projectname, setProjectname] = useState("");
   const navigate = useNavigate();
-  const {projectId}=useParams()
+  const { projectId } = useParams()
 
   console.log(projectId)
   useEffect(() => {
@@ -40,29 +40,16 @@ const ProjectProgress = ({ progress, statisticsData, tasks }) => {
       });
   }, [projectId]);
 
-
- 
   const [hovered, setHovered] = useState(false);
-
-  
   const [loading, setLoading] = useState(false);
   const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+  const successPopupAnimation = useSpring({
+    opacity: showSuccessPopup ? 1 : 0,
+    transform: `scale(${showSuccessPopup ? 1 : 0.5})`,
+  });
 
   const handleDeletionProject = async () => {
-
     navigate(`/updateproject/${projectId}`)
-    // setLoading(true);
-
-    // // Simulate asynchronous operation (API call, etc.)
-    // setTimeout(() => {
-    //   setLoading(false);
-    //   setShowSuccessPopup(true);
-
-    //   // Reset success message after a few seconds
-    //   setTimeout(() => {
-    //     setShowSuccessPopup(false);
-    //   }, 3000);
-    // }, 2000);
   };
 
   const navigateToStatistics = () => {
@@ -81,15 +68,8 @@ const ProjectProgress = ({ progress, statisticsData, tasks }) => {
     navigate(`/ganttchart/${projectId}`);
   };
 
-
-  const successPopupAnimation = useSpring({
-    opacity: showSuccessPopup ? 1 : 0,
-    transform: `scale(${showSuccessPopup ? 1 : 0.5})`,
-  });
-
   return (
     <div style={{ display: 'flex', height: '100vh' }}>
-      {/* Sidebar */}
       <Navigation />
 
       <div style={{ width: '73%', padding: '20px', marginLeft: '350px' }}>
@@ -104,12 +84,10 @@ const ProjectProgress = ({ progress, statisticsData, tasks }) => {
           className="mt-[95px] ml-[50px] sm:text-3xl md:text-[3px] text-[34px] text-left text-indigo-800"
           size="txtPoppinsBold34"
         >
-         {projectname}
+          {projectname}
         </Text>
 
-
         <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginRight: '50px' }}>
-          {/* Update Button */}
           <Button
             className="common-pointer cursor-pointer leading-[normal] min-w-[10px] mt-2.5 text-base text-center tracking-[0.44px]"
             style={{ width: '100px', marginLeft: '50px' }}
@@ -119,8 +97,6 @@ const ProjectProgress = ({ progress, statisticsData, tasks }) => {
           >
             Update
           </Button>
-
-          {/* Delete Button */}
           <Button
             className="common-pointer cursor-pointer leading-[normal] min-w-[10px] mt-2.5 text-base text-center tracking-[0.44px]"
             style={{ width: '100px', marginLeft: '50px', backgroundColor: '#BE3144', color: '#ffffff' }}
@@ -131,14 +107,12 @@ const ProjectProgress = ({ progress, statisticsData, tasks }) => {
           </Button>
         </div>
 
-        {/* Animated Pop-up */}
         {loading && (
           <div className="absolute inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
             <Img className="h-[100px] w-[100px]" src="images/loading.gif" alt="Loading" />
           </div>
         )}
 
-        {/* Success Popup */}
         <animated.div
           style={successPopupAnimation}
           className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-green-500 text-white p-4 rounded-md"
@@ -153,13 +127,9 @@ const ProjectProgress = ({ progress, statisticsData, tasks }) => {
           <h3 onClick={navigateToGantt} style={{ margin: '0 40px', padding: '10px 20px', borderBottom: '3px solid transparent', transition: 'border-bottom-color 0.3s', fontWeight: 'normal', color: '#323F73', cursor: 'pointer' }}>Gantt View</h3>
         </div>
 
-
-
       </div>
     </div>
   );
 };
-
-
 
 export default ProjectProgress;
