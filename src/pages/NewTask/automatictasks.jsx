64bffb6891db3fe4edd2782884938dd44b7046cc
AbdulRehman1891 <114077449+AbdulRehman1891@users.noreply.github.com>
2024-03-14 05:46:54 +0500

    Integrated with Backend

--- src/pages/NewTask/automatictasks.jsx
+++ src/pages/NewTask/automatictasks.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Text } from "components";
+import axios from 'axios';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useParams } from "react-router-dom";
 import { faMicrophone, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useSpring, animated } from "react-spring";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
@@ -8,8 +11,65 @@ const AutomaticTasks = () => {
     const [isMicrophoneClicked, setMicrophoneClicked] = useState(true);
     const [isRecording, setRecording] = useState(false);
     const [tasks, setTasks] = useState([]);
-    const [isLoading, setLoading] = useState(false); // Add loading state
+    const [isLoading, setLoading] = useState(false);
     const { transcript, resetTranscript } = useSpeechRecognition();
+    const { projectId } = useParams();
+    const [projid, setprojid] = useState("");
+    const [projects, setProjects] = useState([]);
+    const [assign, setAssign] = useState([]);
+    const [proj, setProj] = useState("");
+
+    useEffect(() => {
+        const id = localStorage.getItem("userid");
+
+        fetch(`http://127.0.0.1:3000/api/v1/users/${id}/projects`, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                const apiProjects = data.data.projects;
+                setProjects(apiProjects);
+            })
+            .catch(error => {
+                console.error("Error fetching projects:", error);
+            });
+    }, []);
+
+    const HandleProjChange = (e) => {
+        const selectedAssigneeId = e.target.value;
+
+        setprojid(selectedAssigneeId);
+
+        fetch(`http://127.0.0.1:3000/api/v1/projects/${selectedAssigneeId}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Project not found");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setAssign(data.data.project.Members);
+                // handle project data if needed
+            })
+            .catch((error) => {
+                console.error("Error fetching project details:", error);
+            });
+
+        setProj(selectedAssigneeId);
+    };
 
     const handleSpeakNowClick = () => {
         if (!isRecording) {
@@ -21,15 +81,8 @@ const AutomaticTasks = () => {
         }
     };
 
-    const handleMicrophoneClick = () => {
-        setMicrophoneClicked(true);
-        if (!isRecording) {
-            handleSpeakNowClick();
-        }
-    };
-
     const handleGenerateTaskClick = () => {
-        setLoading(true); // Set loading to true when fetching data
+        setLoading(true);
         fetch('http://localhost:8000/api/generate-tasks', {
             method: 'POST',
             headers: {
@@ -41,31 +94,70 @@ const AutomaticTasks = () => {
         })
             .then(response => response.json())
             .then(data => {
-                const tasking = data.tasks;
+                const tasking = data.tasks.map(task => ({ ...task, isSelected: false }));
                 setTasks(tasking);
             })
             .catch(error => {
                 console.error('Error:', error);
             })
             .finally(() => {
-                setLoading(false); // Set loading to false when data fetching is done
+                setLoading(false);
             });
     };
 
+    const handleTaskCheckboxChange = (index) => {
+        setTasks(prevTasks => {
+            const updatedTasks = [...prevTasks];
+            updatedTasks[index] = { ...updatedTasks[index], isSelected: !updatedTasks[index].isSelected };
+            return updatedTasks;
+        });
+    };
+
     const handleConfirmClick = () => {
-        alert('Tasks have been confirmed');
-    }
+        const selectedTasks = tasks.filter(task => task.isSelected);
+
+        const requestData = {
+            tasks: selectedTasks.map(task => ({
+                name: task.name,
+                description: task.description,
+                priority: task.priority,
+                project: projid ? projid : projectId.toString(),
+                assignee:assign[Math.floor(Math.random() * assign.length)],
+            })),
+        };
+        console.log(requestData)
+        let token = localStorage.getItem("token");
+        axios
+            .post("http://127.0.0.1:3000/api/v1/tasks", requestData, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+        })
+            .then(response => {
+                if (response.status === 201) {
+                    alert('Tasks have been confirmed and saved');
+                    setTasks([]);
+                    setMicrophoneClicked(true);
+                } else {
+                    throw new Error('Failed to save tasks');
+                }
+            })
+            .catch(error => {
+                console.error('Error saving tasks:', error);
+                alert('Failed to save tasks. Please try again later.');
+            });
+    };
 
     const handleCancelClick = () => {
-        setTasks([]); // Clear tasks array
-        setMicrophoneClicked(true); // Close the animated div
+        setTasks([]);
+        setMicrophoneClicked(true);
     }
 
     return (
         <div>
-            {/* Microphone Modal */}
             {isMicrophoneClicked && (
-                <animated.div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '9999', backgroundColor: 'white', padding: '40px', borderRadius: '20px', minWidth: '400px', maxWidth: '600px', overflow: 'auto', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
+                <animated.div style={{ position: 'fixed', top: '30%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '9999', backgroundColor: 'white', padding: '40px', borderRadius: '20px', minWidth: '400px', maxWidth: '600px', overflow: 'auto' }}>
                     <button onClick={() => setMicrophoneClicked(false)} className="text-indigo-800 text-lg cursor-pointer absolute top-0 right-0">
                         <FontAwesomeIcon icon={faTimes} />
                     </button>
@@ -86,7 +178,6 @@ const AutomaticTasks = () => {
                         </button>
                     )}
 
-                    {/* Display the list of tasks */}
                     <div>
                         <h3 style={{ textAlign: "center", fontSize: "16px", margin: "20px", fontFamily: 'Poppins', color: "indigo" }}>Here is the List of Tasks Generated</h3>
                         <table style={{ borderCollapse: 'collapse', width: '100%', borderRadius: "4px", fontFamily: 'Poppins' }}>
@@ -105,13 +196,31 @@ const AutomaticTasks = () => {
                                         <td style={{ padding: '8px', textAlign: 'left' }}>{task.priority}</td>
                                         <td style={{ padding: '8px', textAlign: 'left' }}>{task.description}</td>
                                         <td style={{ padding: '8px', textAlign: 'left' }}>
-                                            <input type="checkbox" />
+                                            <input
+                                                type="checkbox"
+                                                checked={task.isSelected}
+                                                onChange={() => handleTaskCheckboxChange(index)}
+                                            />
                                         </td>
                                     </tr>
                                 ))}
                             </tbody>
                         </table>
                     </div>
+
+                    {projectId === '123456' && (
+                        <div className="flex md:flex-col flex-row md:gap-10 items-start justify-between mt-[34px] w-[97%] md:w-full">
+                            <Text className="md:mt-0 mt-0.5 text-base text-indigo-800 tracking-[0.44px]" size="txtPoppinsRegular16">Select Project</Text>
+                            <div className="text-base w-[76%]" style={{ backgroundColor: 'transparent' }}>
+                                <select name="project" value={proj} onChange={HandleProjChange} style={{ fontSize: '1rem', width: '100%', backgroundColor: 'transparent', border: 'none', outline: 'none', borderBottom: '0.5px solid #1F2544' }}>
+                                    <option>Select Project</option>
+                                    {projects.map((member) => (
+                                        <option key={member.id} value={member.id}>{member.name}</option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+                    )}
 
                     <button onClick={handleCancelClick} style={{ backgroundColor: '#B80000', color: '#FFFFFF', fontSize: '1.25rem', padding: '0.5rem 1rem', borderRadius: '20px', marginTop: '0.5rem', marginLeft: "100px" }}>
                         Cancel
