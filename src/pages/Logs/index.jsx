029377d27ecca5f041bbb7f33e0091fc38b7fab0
AbdulRehman1891 <114077449+AbdulRehman1891@users.noreply.github.com>
2024-03-28 12:48:26 +0500

    Logs Added

--- src/pages/Logs/index.jsx
+++ src/pages/Logs/index.jsx
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from '@mui/material';
+import { Box, Paper, Typography, Divider, Button } from '@mui/material';
+import { useNavigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
+
+// Define a theme with spacing and colors
+const theme = createTheme({
+  spacing: 2,
+  palette: {
+    primary: {
+      main: '#1976d2', // Adjust color as needed
+    },
+    secondary: {
+      main: '#f50057', // Adjust color as needed
+    },
+  },
+});
 
 const LogsTable = () => {
   const [logs, setLogs] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchLogs = async () => {
@@ -28,57 +44,112 @@ const LogsTable = () => {
     fetchLogs();
   }, []);
 
+  const handleLogout = async () => {
+    try {
+      let token = localStorage.getItem('token');
+      const response = await fetch("http://127.0.0.1:3000/api/v1/users/logout", {
+        method: "GET",
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
+        localStorage.removeItem('email');
+        localStorage.removeItem('photo');
+        navigate("/signin");
+      } else {
+        console.error("Error logging out:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead style={{ backgroundColor: '#F7F1E5' }}>
-          <TableRow>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">Previous Data</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">New Data</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">Updated By</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">Task ID</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">Project ID</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">User ID</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">Created At</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">Updated At</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography variant="subtitle1" align="center">Type of Request</Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {logs.map(log => (
-            <TableRow key={log._id}>
-              <TableCell>{JSON.stringify(log.prevData)}</TableCell>
-              <TableCell>{JSON.stringify(log.newData)}</TableCell>
-              <TableCell>{log.updatedBy}</TableCell>
-              <TableCell>{log.taskId}</TableCell>
-              <TableCell>{log.projectId}</TableCell>
-              <TableCell>{log.userId}</TableCell>
-              <TableCell>{new Date(log.createdAt).toLocaleString()}</TableCell>
-              <TableCell>{new Date(log.updatedAt).toLocaleString()}</TableCell>
-              <TableCell>{log.typeofRequest}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <ThemeProvider theme={theme}>
+      <Box p={4}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h4" gutterBottom>
+            ProjectFlow Logs
+          </Typography>
+          <Button color="primary" variant="outlined" onClick={handleLogout}>Logout</Button>
+        </Box>
+        <Divider />
+        {logs.map((log, index) => (
+          <Paper key={log._id} elevation={3} style={{ backgroundColor: index % 2 === 0 ? '#f3f3f3' : '#e0e0e0', padding: '16px', margin: '16px 0', borderRadius: '8px' }}>
+            {log.prevData && (
+              <Box maxWidth="100%" overflow="auto">
+                <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>Previous Data</Typography>
+                <Typography variant="body1" gutterBottom>{JSON.stringify(log.prevData)}</Typography>
+              </Box>
+            )}
+            {log.newData && (
+              <Box maxWidth="100%" overflow="auto">
+                <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>New Data</Typography>
+                <Typography variant="body1" gutterBottom>{JSON.stringify(log.newData)}</Typography>
+              </Box>
+            )}
+            {log.updatedBy && (
+              <>
+                <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>Updated By</Typography>
+                <Typography variant="body1" gutterBottom>{log.updatedBy}</Typography>
+              </>
+            )}
+            {log.taskId && (
+              <>
+                <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>Task ID</Typography>
+                <Typography variant="body1" gutterBottom>{log.taskId}</Typography>
+              </>
+            )}
+            {log.projectId && (
+              <>
+                 <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>Project ID</Typography>
+                 <Typography variant="body1" gutterBottom>{log.projectId}</Typography>
+              </>
+            )}
+            {log.userId && (
+              <>
+                  <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>User ID</Typography>
+                  <Typography variant="body1" gutterBottom>{log.userId}</Typography>
+              </>
+            )}
+            {log.typeofRequest==="create" && (
+              <>
+                <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>Created At</Typography>
+            <Typography variant="body1" gutterBottom>{new Date(log.createdAt).toLocaleString()}</Typography>
+              </>
+            )}
+            {log.typeofRequest==="update" && (
+              <>
+                <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>Updated At</Typography>
+                <Typography variant="body1" gutterBottom>{new Date(log.updatedAt).toLocaleString()}</Typography>
+              </>
+            )}
+            <Typography variant="subtitle1" gutterBottom style={{ fontWeight: 'bold', fontSize: '1.2rem', color: theme.palette.primary.main }}>Type of Request</Typography>
+            <Typography
+              variant="body1"
+              gutterBottom
+              style={{
+                color:
+                  log.typeofRequest === 'create'
+                    ? 'green'
+                    : log.typeofRequest === 'delete'
+                    ? 'red'
+                    : log.typeofRequest === 'update'
+                    ? 'orange'
+                    : 'inherit', // Fallback to default color
+              }}
+            >
+              {log.typeofRequest}
+          </Typography>
+
+          </Paper>
+        ))}
+      </Box>
+    </ThemeProvider>
   );
 };
 
